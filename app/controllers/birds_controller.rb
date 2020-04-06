355d56be888fb80aef0ff3046caf4ef9df9f828b
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-06 15:23:29 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,6 +6,10 @@ class BirdsController < ApplicationController
   
   def show
     bird = Bird.find_by(id: params[:id])
-    render json: bird.slice(:id, :name, :species)
+    if bird
+      render json: bird.slice(:id, :name, :species)
+    else
+      render :json: { message: 'Bird not found'}
+      
   end
 end
